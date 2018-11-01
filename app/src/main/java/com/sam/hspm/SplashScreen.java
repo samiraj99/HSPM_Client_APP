610cd7b572710c47dcd8846c883d3872ebd0c5e7
Samiraj Shirsat <36511946+samiraj99@users.noreply.github.com>
2018-11-01 17:45:56 +0530

    Splash Activity
    Setting up App adding logo, theme and Update Splash Activity.

--- app/src/main/java/com/sam/hspm/SplashScreen.java
+++ app/src/main/java/com/sam/hspm/SplashScreen.java
@@ -1,7 +1,9 @@
 package com.sam.hspm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new CountDownTimer(3000, 500) {
+            @Override
+            public void onTick(long l) {
+
+            }
+            @Override
+            public void onFinish() {
+                Intent login = new Intent(SplashScreen.this,LoginScreen.class);
+                startActivity(login);
+                finish();
+            }
+        }.start();
     }
 }
