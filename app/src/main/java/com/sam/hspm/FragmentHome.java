10018ffc1b894edfbf1adcdc2b23d12b08ad1908
Samiraj Shirsat <36511946+samiraj99@users.noreply.github.com>
2018-11-03 19:25:57 +0530

    FragmentHome
    FragmentHome page is created added card view for adertise offers and buttons for hw and sw.

--- app/src/main/java/com/sam/hspm/FragmentHome.java
+++ app/src/main/java/com/sam/hspm/FragmentHome.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class FragmentHome extends Fragment {
+
+     private RecyclerView mRecyclerView;
+     private RecyclerView.LayoutManager mLayoutManager;
+     private RecyclerView.Adapter mAdapter;
+     private ArrayList<String> mDataset;
+     View v1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        v1 = inflater.inflate(R.layout.fragment_home,container,false);
 
+        mDataset = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            mDataset.add("New title "+i);
+        }
+
+        mRecyclerView = v1.findViewById(R.id.Recycler_View);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MainAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return v1;
     }
 }
---
+++ app/src/main/java/com/sam/hspm/MainAdapter.java
@@ -0,0 +1,52 @@
+package com.sam.hspm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    private ArrayList<String> mDList;
+
+    public MainAdapter(ArrayList<String> mDList) {
+        this.mDList = mDList;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_ad, viewGroup, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
+        viewHolder.mTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Tag", "Id Click" + mDList.get(i));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTitle = itemView.findViewById(R.id.TextView_Recycler);
+        }
+    }
+}
